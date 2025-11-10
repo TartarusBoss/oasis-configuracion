b1f0fa11a39b485572c5efa018db6da45eecc67a
TartarusBoss <matiasherreravanegas26@gmail.com>
2025-11-10 00:46:18 -0500

    fix: Update Jest config and frontend Vite setup

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,7 @@
 export default {
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
+  testEnvironment: 'node',
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  testEnvironment: 'node'
+  }
 };
